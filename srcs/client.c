11a1a61b885a922be8b1b4b73ba68997e0904318
timofeykafanov <tim.kafanov@yandex.ru>
2024-07-05 14:31:24 +0200

    feat: try to send number first

--- srcs/client.c
+++ srcs/client.c
@@ -6,13 +6,13 @@
 /*   By: tkafanov <tkafanov@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 10:34:28 by tkafanov          #+#    #+#             */
-/*   Updated: 2024/07/04 14:45:24 by tkafanov         ###   ########.fr       */
+/*   Updated: 2024/07/05 12:34:41 by tkafanov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-bool	g_response_received = false;
+bool	g_response_received;
 
 static bool	is_process_id_valid(char *pid_str, int process_id)
 {
@@ -45,16 +45,54 @@ static void	response(int signo)
 
 static void	send_symbol(int process_id, char symbol)
 {
-	int i;
+	int 		i;
 
 	i = 7;
 	while (i >= 0)
 	{
 		ft_printf("%d", STDOUT_FILENO, (symbol >> i) % 2);
 		if ((symbol >> i) % 2 == 0)
-			kill(process_id, SIGUSR1);
+		{
+			if (kill(process_id, SIGUSR1) == -1)
+				exit(1);
+		}
 		else
-			kill(process_id, SIGUSR2);
+		{
+			if (kill(process_id, SIGUSR2) == -1)
+				exit(1);
+		}
+		i--;
+		g_response_received = false;
+		if (!g_response_received)
+		{
+			sleep(10);
+			if (!g_response_received)
+			{
+				ft_printf("Error! No response was received from the server!\n", STDERR_FILENO);
+				exit(1);
+			}
+		}
+	}
+}
+
+static void	send_len(int process_id, int len)
+{
+	int	i;
+
+	i = 31;
+	while (i >= 0)
+	{
+		ft_printf("%d", STDOUT_FILENO, (len >> i) % 2);
+		if ((len >> i) % 2 == 0)
+		{
+			if (kill(process_id, SIGUSR1) == -1)
+				exit(1);
+		}
+		else
+		{
+			if (kill(process_id, SIGUSR2) == -1)
+				exit(1);
+		}
 		i--;
 		g_response_received = false;
 		if (!g_response_received)
@@ -71,6 +109,7 @@ static void	send_symbol(int process_id, char symbol)
 
 static void	send_message(int process_id, char *message)
 {
+	send_len(process_id, ft_strlen(message));
 	while (*message)
 	{
 		send_symbol(process_id, *message);
--- srcs/server.c
+++ srcs/server.c
@@ -6,38 +6,45 @@
 /*   By: tkafanov <tkafanov@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 10:34:34 by tkafanov          #+#    #+#             */
-/*   Updated: 2024/07/04 16:12:45 by tkafanov         ###   ########.fr       */
+/*   Updated: 2024/07/05 14:16:46 by tkafanov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
+#include <unistd.h>
 
 t_data	g_data;
 
 void	sig_usr(int signo, siginfo_t *info, void *useless)
 {
-	static int	count = 0;
-	static char	symbol = 0;
+	// static int	count = 0;
+	// static char	symbol = 0;
 
 	(void)useless;
+	// (void)signo;
+	usleep(1000);
+	kill(info->si_pid, SIGUSR1);
+	// if (g_data.bits <= 32)
+	// 	return ;
+
 	if (signo == SIGUSR1)
 	{
-		symbol = symbol << 1;
-		kill(info->si_pid, SIGUSR1);
-		count++;
+		g_data.last_bit = 0;
+		// symbol = symbol << 1;
+		// count++;
 	}
 	else if (signo == SIGUSR2)
 	{
-		symbol = (symbol << 1) | 1;
-		kill(info->si_pid, SIGUSR1);
-		count++;
+		g_data.last_bit = 1;
+		// symbol = (symbol << 1) | 1;
+		// count++;
 	}
-	if (count == 8)
-	{
-		ft_printf("%c", STDOUT_FILENO, symbol);
-		count = 0;
-		symbol = 0;
-	}
+	// if (count == 8)
+	// {
+	// 	ft_printf("%c", STDOUT_FILENO, symbol);
+	// 	count = 0;
+	// 	symbol = 0;
+	// }
 }
 
 int	main(void)
@@ -56,11 +63,16 @@ int	main(void)
 		return (ERROR);
 	while (1)
 	{
-		g_data.bits++;
+		if (g_data.bits > 32)
+			return (0);
 		if (g_data.bits <= 32)
 		{
-			g_data.size = (g_data.size * 2) + g_data.last_bit;
+			ft_printf("last bit = %d\n", STDOUT_FILENO, g_data.last_bit);
+			g_data.size = g_data.size * 2 + g_data.last_bit;
 		}
+		ft_printf("len = %d\n", STDOUT_FILENO, g_data.size);
+		ft_printf("bits = %d\n", STDOUT_FILENO, g_data.bits);
+		g_data.bits++;
 		pause();
 	}
 	return (SUCCESS);
